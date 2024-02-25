3ad8507ece78c2a0d281d17710d7146974bd0813
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-02-25 20:12:16 +0530

    learning of Datatypes

---
+++ 02_Datatype.js
@@ -0,0 +1,20 @@
+"use strict" // this helps you treat all js codes as newer version 
+
+let name = "Harsh" //this is a string 
+let age = 20 // this is a integer 
+
+//DATA TYPES
+
+//"" is used to define a string
+
+
+//null it is a standalone value 
+//undefined 
+
+// Difference between null and undefined
+// null is a representation to define that it is empty 
+// undefined means there is no value assigned
+
+//symbol = unique
+
+console.log(typeof age)
