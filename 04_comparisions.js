7acad79114d82bc9be215e884a2ac62b24aba1c1
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-03-04 21:32:50 +0530

    Comparious

---
+++ 04_comparisions.js
@@ -0,0 +1,22 @@
+console.log(2>1)
+
+//Comparision of different datatypes
+console.log("2">1);
+console.log("02">1);
+
+
+//The reason for the solution is that == is a equality check and adding > is a comparison check so when we add <> signes 
+//so when we do comparison null is convertd into zero that is why in the last one it converted to 0 and we get the value as true
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+//all the answers will remain same as undefined has no defined values 
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+
+console.log("2"==2);
+console.log("2"===2);
