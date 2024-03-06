3b9e86d436526d2099b2d8d548f04e005cf11bb2
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-03-07 02:16:15 +0530

    New learning Basics of arrays and functions

---
+++ 05_Datatypes_summary.js
@@ -0,0 +1,33 @@
+// #Primitive 
+// There are 7 types of primitive datatype they are call by value Strings, Number, Boolean, Null, Undefined, Symbols, BigInt
+
+// #Reference (Non-Primitive)
+//Array, Objects, Functions
+
+
+
+// JavaScript is a Dynamically typed language
+// Dynamically typed- we dont need to specify the type of variable
+// Statiscally typed - we have to specify the type of the variable
+
+
+// NON-Primitive Data types
+//-->ARRAY
+
+const arrayofheros =["Harsh","Vardhan","Upadhyay"]
+
+//we use {} to add objects in an array
+let myObj = 
+{
+    name: "Harsh",
+    number: 31,
+    idNumber: "RA2111033010031"
+}
+
+
+// FUNCTIONS
+const myfunction = function(){
+    console.log("hello world")
+}
+
+myfunction()
