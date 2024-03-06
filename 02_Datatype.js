924c8eb19e41b47fbbd7909d2e0556328f9b593d
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-03-07 02:04:48 +0530

    learning the use of symbol

--- 02_Datatype.js
+++ 02_Datatype.js
@@ -19,3 +19,19 @@ let age = 20 // this is a integer
 
 console.log(typeof age)
 // this is used to know what kind of a datatype it is being used 
+
+
+
+//Smymbol are unique even if they have same value 
+const id = Symbol('1232')
+const anotherId = Symbol('1232')
+
+console.log(id)
+console.log(anotherId);
+console.log(id == anotherId)
+//As you can see even though the values are same we still have this statement as false as when we use symbol they are unique
+
+
+const bignumber = (948710948702935n);
+console.log(typeof bignumber)
+
