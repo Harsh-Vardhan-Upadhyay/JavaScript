bf43fbe6f0790710a96a25cb813e6fb45fb8e147
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-03-20 21:12:34 +0530

    new strings learnings

--- 07_strings.js
+++ 07_strings.js
@@ -4,5 +4,14 @@
  console.log(name + repoCount)
 
 //Modern way of using this samething which makes your code more easy to understand 
-
 console.log(`My Name is ${name} and my github reposiotory count is ${repoCount}`)
+
+
+//Another way to declare a string
+const gameName = new String('harsh')
+
+console.log(gameName[2]);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length) // this is the syntax used to check the length of the string
+console.log(gameName.toUpperCase) // this is used to change the characters to upper case values without changing the original strings values as stack memory is used in here for string cause it is primitive
