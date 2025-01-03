c0853d50a018f7ac9cdb92b774db7be43567a532
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2025-01-04 01:41:47 +0530

    :alien: Learning function in JavaScript

--- 12_functions.js
+++ 12_functions.js
@@ -35,3 +35,46 @@ function userMessage(user) {
 }
 
 console.log(userMessage("Harsh"))
+
+
+
+// A function to calculate the total cart price by dynamically accepting any number of arguments
+function totalCartPrice(...num1) {
+    // 'num1' is a rest parameter that captures all passed arguments into an array
+    return num1; // This currently just returns the array of prices without calculating the total
+}
+
+// Calling the function with multiple price values to test dynamic intake
+console.log(totalCartPrice(200, 200, 300)); // This will log: [200, 200, 300]
+
+
+// Defining an object to represent a user
+const user = {
+    userName: "Harsh", // The user's name (note: it must be a string, enclosed in quotes)
+    userId: 31         // The user's ID
+};
+
+// Defining a function to work with objects
+function handleObject(anyObj) {
+    // Accessing properties of the object passed as an argument using dot notation
+    return `The user name is ${anyObj.userName} and their ID is ${anyObj.userId}`;
+}
+
+// Calling the function with the 'user' object and logging the result
+console.log(handleObject(user)); 
+// Expected output: "The user name is Harsh and their ID is 31"
+
+
+// Defining an array 
+const myNewArray = [1, 2, 3]; 
+
+// Defining a function that takes an array as input
+function returnSecondValue(getArray) {
+    // Accessing the second element of the array using its index (1) 
+    // and returning it as the function's output
+    return getArray[1];
+}
+
+// Calling the function with 'myNewArray' as the argument
+// This passes the array to the function where the second element is accessed
+console.log(returnSecondValue(myNewArray)); 
