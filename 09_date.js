582188fb31be59a2fe4ee4435185233ff08b8f16
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2025-01-03 14:39:35 +0530

    :checkered_flag: Learnt about dates in Javascript

--- 09_date.js
+++ 09_date.js
@@ -1,6 +1,15 @@
-let myDate = new Date()
-console.log(myDate.toDateString())
+// Create a new Date object representing the current date and time
+// The 'new' keyword is used to create an instance of the Date object
+let myDate = new Date(); 
 
+// Convert the date to a readable string format (e.g., "Thu Jan 02 2025") and log it to the console
+console.log(myDate.toDateString()); 
 
-let myCreatedDate = new Date(2023,0,23)// Months in javaScript start from 0
-console.log(myCreatedDate.toDateString())
+// Create a new Date object for a specific date
+// The 'new' keyword is required to instantiate a new Date object with the given parameters
+// The parameters for the Date constructor are (year, month, day)
+// Note: Months in JavaScript are zero-indexed (0 = January, 1 = February, ..., 11 = December)
+let myCreatedDate = new Date(2023, 0, 23); 
+
+// Convert the specified date to a readable string format and log it to the console
+console.log(myCreatedDate.toDateString()); 
